9f47ffc296124991d6c08588a9c53af79e0c7978
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2022-11-10 16:11:11 +0000

    11/10/2022, 11:11:11 PM

--- src/controller/AlgorithmController.ts
+++ src/controller/AlgorithmController.ts
@@ -34,7 +34,7 @@ export class AlgorithmController {
   ) {}
   @Get('/generate')
   generateListStudents(@Query('n') n: number, @Query('q') q: number) {
-    let result: any[][][] = [];
+    let result: any = [];
     for (let i = 0; i < n; i++) {
       for (let j = 0; j < q; j++) {
         result[i][j][0] = Math.floor(Math.random() * 1000);
