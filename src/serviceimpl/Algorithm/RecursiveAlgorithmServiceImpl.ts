db1bf2cf4a76a95c08f08ff610678ce6317b34db
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2022-11-10 16:02:56 +0000

    11/10/2022, 11:02:56 PM

--- src/serviceimpl/Algorithm/RecursiveAlgorithmServiceImpl.ts
+++ src/serviceimpl/Algorithm/RecursiveAlgorithmServiceImpl.ts
@@ -8,4 +8,15 @@ export class RecursiveAlgorithmServiceImpl
   RecursiveBinarySum(list: any, recursiveFunction: any): number {
     return recursiveFunction(0, list.length - 1, list);
   }
+  recursiveFunction(low: number, high: number, list: any[][]): number {
+    if (low == high) return list[low][1];
+    else {
+      const mid = Math.floor((low + high) / 2);
+
+      return (
+        this.recursiveFunction(low, mid, list) +
+        this.recursiveFunction(mid + 1, high, list)
+      );
+    }
+  }
 }
