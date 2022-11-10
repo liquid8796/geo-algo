d6078eb36f52117dd2da87440c09e601ff5554e4
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2022-11-10 16:20:37 +0000

    11/10/2022, 11:20:37 PM

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './config/AppModule';
 import { TimeoutInterceptor } from './config/TimeoutInterceptor';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   app.useGlobalInterceptors(new TimeoutInterceptor());
   //const app = await NestFactory.create<NestExpressApplication>(AppModule); //you don't need to specify a type unless you actually want to access the underlying platform API
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000);
 }
 bootstrap();
