66a44d0592901c11e42e2297ae0e20b9a14ce65b
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2022-11-10 16:18:52 +0000

    11/10/2022, 11:18:52 PM

--- src/controller/AlgorithmController.ts
+++ src/controller/AlgorithmController.ts
@@ -39,12 +39,6 @@ export class AlgorithmController {
       result[i] = [];
       for (let j = 0; j < q; j++) {
         result[i][j] = [];
-        result[i][j][0] = 0;
-        result[i][j][1] = 0;
-      }
-    }
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < q; j++) {
         result[i][j][0] = Math.floor(Math.random() * 1000);
         result[i][j][1] = Math.floor(Math.random() * 11);
       }
