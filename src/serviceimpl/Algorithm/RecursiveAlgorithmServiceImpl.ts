0fa667eff4fe1b4e2dbc61e16a30058161f00931
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2022-11-10 16:03:41 +0000

    11/10/2022, 11:03:41 PM

--- src/serviceimpl/Algorithm/RecursiveAlgorithmServiceImpl.ts
+++ src/serviceimpl/Algorithm/RecursiveAlgorithmServiceImpl.ts
@@ -6,17 +6,21 @@ export class RecursiveAlgorithmServiceImpl
   implements RecursiveAlgorithmService
 {
   RecursiveBinarySum(list: any): number {
-    return this.recursiveFunction(0, list.length - 1, list);
+    return this.StudentPointRecursiveFunction(0, list.length - 1, list);
   }
 
-  private recursiveFunction(low: number, high: number, list: any[][]): number {
+  private StudentPointRecursiveFunction(
+    low: number,
+    high: number,
+    list: any[][],
+  ): number {
     if (low == high) return list[low][1];
     else {
       const mid = Math.floor((low + high) / 2);
 
       return (
-        this.recursiveFunction(low, mid, list) +
-        this.recursiveFunction(mid + 1, high, list)
+        this.StudentPointRecursiveFunction(low, mid, list) +
+        this.StudentPointRecursiveFunction(mid + 1, high, list)
       );
     }
   }
